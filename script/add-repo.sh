d129de4ca8395a02fe739e0f1df9476b3c084c0b
Febri Hardiansyah <febrihardians@gmail.com>
2019-01-03 17:22:42 +0700

    add java and ant

--- script/add-repo.sh
+++ script/add-repo.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+
 add-apt-repository ppa:ondrej/nginx
 add-apt-repository ppa:ondrej/php
--- script/config.sh
+++ script/config.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+
 rm /etc/nginx/sites-available/default
 cp ./nginx/sites-available/default /etc/nginx/sites-available/
